1343b696cd1dd6817587c18397717610599b75b5
patrickchow0803 <patrickchow0803@gmail.com>
2020-12-07 19:32:50 -0500

    Fixed Media Query bug

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import 'package:product_to_do/screens/login.dart';
 import 'package:product_to_do/services/auth.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(App());
 }
 
@@ -18,31 +19,34 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      // Initialize FlutterFire:
-      future: _initialization,
-      builder: (context, snapshot) {
-        // Check for errors
-        if (snapshot.hasError) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: FutureBuilder(
+        // Initialize FlutterFire:
+        future: _initialization,
+        builder: (context, snapshot) {
+          // Check for errors
+          if (snapshot.hasError) {
+            return const Scaffold(
+              body: Center(
+                child: Text('Error'),
+              ),
+            );
+          }
+
+          // Once complete, show your application
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Root();
+          }
+
+          // Otherwise, show something whilst waiting for initialization to complete
           return const Scaffold(
             body: Center(
-              child: Text('Error'),
+              child: CircularProgressIndicator(),
             ),
           );
-        }
-
-        // Once complete, show your application
-        if (snapshot.connectionState == ConnectionState.done) {
-          return Root();
-        }
-
-        // Otherwise, show something whilst waiting for initialization to complete
-        return const Scaffold(
-          body: Center(
-            child: CircularProgressIndicator(),
-          ),
-        );
-      },
+        },
+      ),
     );
   }
 }
@@ -66,10 +70,16 @@ class _RootState extends State<Root> {
         if (snapshot.connectionState == ConnectionState.active) {
           if (snapshot.data?.uid == null) {
             // login screen
-            return Login();
+            return Login(
+              auth: _auth,
+              firestore: _firestore,
+            );
           } else {
             // register screen
-            return Home();
+            return Home(
+              auth: _auth,
+              firestore: _firestore,
+            );
           }
         } else {
           return const Scaffold(
